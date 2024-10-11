9ea25aceaad976a94a9670a2f30e551898cb5fe0
bansalamol <amolbansal15@gmail.com>
2024-10-12 00:39:54 +0530

    sitemap enable

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // REQUIRED: add your own domain name here (e.g. https://ipoexpert.io),
-  siteUrl: process.env.SITE_URL || "https://example.in",
-  generateRobotsTxt: false,
+  siteUrl: process.env.SITE_URL || "https://www.ipoexpert.io",
+  generateRobotsTxt: true,
   // use this to exclude routes from the sitemap (i.e. a user dashboard). By default, NextJS app router metadata files are excluded (https://nextjs.org/docs/app/api-reference/file-conventions/metadata)
   exclude: ["/twitter-image.*", "/opengraph-image.*", "/icon.*"],
 };
